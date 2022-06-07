bce1666a56b96b54f00e9b009af030ae4dede612
rafaldziuryk <rafal.dziuryk@appvinio.com>
2022-06-07 07:23:46 +0200

    2 Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -27,57 +27,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedItem = 1;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[Colors.black, Colors.blue]),
-            ),
-          ),
-          title: Text('Tutorial'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.calendar_month),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.calendar_today),
-              onPressed: () {},
-            ),
-          ],
-          leading: IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
-          ),
-          bottom: TabBar(
-            tabs: [
-              Text('TAB1'),
-              Text('TAB2'),
-              Text('TAB3'),
-            ],
-          ),
+        appBar: appbar(),
+        body: body(),
+        bottomNavigationBar: bottomNavigationBar(),
+      ),
+    );
+  }
+
+  BottomNavigationBar bottomNavigationBar() {
+    return BottomNavigationBar(
+      // type: BottomNavigationBarType.shifting,
+      // showSelectedLabels: false,
+      // showUnselectedLabels: true,
+      currentIndex: selectedItem,
+      onTap: (index) {
+        setState(() {
+          selectedItem = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          backgroundColor: Colors.red,
+          activeIcon: Icon(Icons.outlined_flag),
+          icon: Icon(Icons.flag),
+          label: 'Flag',
         ),
-        body: TabBarView(
-          children: [
-            Center(
-              child: Text('Tab'),
-            ),
-            Center(
-              child: Text('Tab'),
-            ),
-            Center(
-              child: Text('Tab'),
-            ),
-          ],
+        BottomNavigationBarItem(
+          backgroundColor: Colors.green,
+          activeIcon: Icon(Icons.bookmark_outline),
+          icon: Icon(Icons.bookmark),
+          label: 'Bookmark',
         ),
+        BottomNavigationBarItem(
+          backgroundColor: Colors.green,
+          activeIcon: Icon(Icons.book_outlined),
+          icon: Icon(Icons.book),
+          label: 'Book',
+        )
+      ],
+    );
+  }
+
+  TabBarView body() {
+    return TabBarView(
+      children: [
+        Center(
+          child: Text('Tab'),
+        ),
+        Center(
+          child: Text('Tab'),
+        ),
+        Center(
+          child: Text('Tab'),
+        ),
+      ],
+    );
+  }
+
+  AppBar appbar() {
+    return AppBar(
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[Colors.black, Colors.blue]),
+        ),
+      ),
+      title: Text('Tutorial'),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.calendar_month),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.calendar_today),
+          onPressed: () {},
+        ),
+      ],
+      leading: IconButton(
+        icon: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      bottom: TabBar(
+        tabs: [
+          Text('TAB1'),
+          Text('TAB2'),
+          Text('TAB3'),
+        ],
       ),
     );
   }
